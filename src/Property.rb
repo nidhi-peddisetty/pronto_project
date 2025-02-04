6ad6ac10ec03dd760761d5c87a88ac281b844a7e
nidhi-peddisetty <103842690@student.swin.edu.au>
2025-02-04 21:50:11 +1100

    [game logic] implemented game logic and added test specs for all classes

--- src/Property.rb
+++ src/Property.rb
@@ -5,7 +5,42 @@ class Property
     @price = price
     @colour = colour
     @type = type
-    @owner = nil
-    @rent = nil #calculate_rent method TBC
+    @owner = nil # Initializing with no owner for the first round
+    @rent = calculate_rent # Method calculate_rent to determine rent for each property
+  end
+
+  def calculate_rent
+    case @price # Calculates rent based on json file's price attribute
+    when 1..4 then @price
+    else 0
+    end
+  end
+
+  def is_owned?
+    !@owner.nil?
+  end
+
+  def pay_rent(player)
+    if is_owned?
+      rent_to_pay = @rent
+      rent_to_pay *= 2 if owner_has_both_properties?(@owner) # Double rent if both coloured properties are owned
+      amount_left = player.money
+      player.pay_rent(rent_to_pay)
+      @owner.money += if amount_left < rent_to_pay
+                        amount_left
+                      else
+                        rent_to_pay
+                      end
+      #In line testing code block to check rent, own money of the player
+      # if player.bankrupt? == false
+      #   puts "#{player.name} pays $#{rent_to_pay} rent to #{@owner.name}"
+      #   puts "#{player.name} has $#{player.money} left and #{@owner.name} has $#{@owner.money} left"
+      # end
+    end
+  end
+
+  def owner_has_both_properties?(owner)
+    owned_properties = owner.properties.select { |prop| prop.colour == @colour }
+    owned_properties.size == 2 # Adjusting the colour group size to 2 based on raw data
   end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,24 @@
-puts 'Hello, Ruby World!'
+require 'json'
+require_relative "Board"
+require_relative "Player"
+require_relative "Game"
+
+def play_game(dice_rolls_file, game_number) #Function to handle multiple dice rolls inputs
+  board_data = JSON.parse(File.read('./data/board.json'))
+  dice_rolls_data = JSON.parse(File.read(dice_rolls_file))
+
+  board = Board.new(board_data)
+
+  #Defining players
+  players = [Player.new("Peter"), Player.new("Billy"), Player.new("Charlotte"), Player.new("Sweedal")]
+
+  game = Game.new(players, board, dice_rolls_data)
+
+  puts "\n---Start of Game Number: #{game_number}---"
+  game.play
+  puts "---End of Game #{game_number}---\n\n"
+end
+
+play_game('./data/rolls_1.json', 1)
+
+play_game('./data/rolls_2.json', 2)
